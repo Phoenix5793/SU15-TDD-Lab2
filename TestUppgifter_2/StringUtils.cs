a3cbc04e50e0849db93e2d18f210035733dd6a7a
Johan Svensson <johan@atomicplayboy.net>
2016-04-21 13:54:37 +0200

    Implemented NextPrime()

--- TestUppgifter_2/StringUtils.cs
+++ TestUppgifter_2/StringUtils.cs
@@ -37,7 +37,36 @@ namespace TestUppgifter_2
 
 		public static int[] NextPrime(int input)
 		{
-			return new int[] {};
+			int[] primes = new int[3];
+			int foundPrimes = 0;
+
+			for (int i = 1; foundPrimes < 3; i++)
+			{
+				int candidate = input + i;
+				if (IsPrime(candidate))
+				{
+					primes[foundPrimes] = candidate;
+					i++;
+					foundPrimes++;
+				}
+			}
+
+			return primes;
+		}
+
+		public static bool IsPrime(int candidate)
+		{
+			int boundary = (int)Math.Sqrt(candidate);
+
+			if (candidate == 1) return false;
+			if (candidate == 2) return true;
+
+			for (int i = 2; i <= boundary; i++)
+			{
+				if (candidate % i == 0) return false;
+			}
+
+			return true;
 		}
 
 	}
