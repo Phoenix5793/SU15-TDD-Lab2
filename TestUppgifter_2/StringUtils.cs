6fd3457992168a9da1eb37690083a0674bab735e
Johan Svensson <johan@atomicplayboy.net>
2016-04-21 12:10:23 +0200

    Implemented StrType()

--- TestUppgifter_2/StringUtils.cs
+++ TestUppgifter_2/StringUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TestUppgifter_2
@@ -15,7 +16,17 @@ namespace TestUppgifter_2
 
 		public static string StrType(string input)
 		{
-			return string.Empty;
+			if (Regex.IsMatch(input, $"^[0-9]+$"))
+			{
+				return "number";
+			}
+
+			if (Regex.IsMatch(input, $"^[a-zA-Z]+$"))
+			{
+				return "string";
+			}
+
+			return "none";
 		}
 
 		public static string NextPalindrome(int input)
