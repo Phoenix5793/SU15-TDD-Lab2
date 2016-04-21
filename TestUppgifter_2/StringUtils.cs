b4f4a0817f835ef57c551c79d4e4eb4177fb3099
Johan Svensson <johan@atomicplayboy.net>
2016-04-21 10:55:40 +0200

    Added StringUtils scaffold

---
+++ TestUppgifter_2/StringUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUppgifter_2
+{
+	public static class StringUtils
+	{
+		public static int StrCount(string input)
+		{
+			return 0;
+		}
+
+		public static string StrType(string input)
+		{
+			return string.Empty;
+		}
+
+		public static string NextPalindrome(int input)
+		{
+			return string.Empty;
+		}
+
+
+		public static int[] NextPrime(int input)
+		{
+			return new int[] {};
+		}
+
+	}
+}
